a9299f4c0a4a278af591d55006ef85ed813a29e7
shen <1932365604@qq.com>
2015-11-22 20:23:22 +0800

    modify BaseDao file

--- src/main/java/org/shen/dao/BaseDao.java
+++ src/main/java/org/shen/dao/BaseDao.java
@@ -5,7 +5,5 @@ import org.shen.entity.BaseEntity;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface BaseDao<T extends BaseEntity> extends Mapper<T>{
-	public void delete(T entity);
-	
 	public void add(T entity);
 }
