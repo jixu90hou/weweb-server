151dc90ac7d8cf3b7a26762ff4cabda8d785e648
shen <1932365604@qq.com>
2015-11-29 16:45:05 +0800

    修改BaseDao操作

--- src/main/java/org/shen/dao/BaseDao.java
+++ src/main/java/org/shen/dao/BaseDao.java
@@ -1,9 +1,26 @@
 package org.shen.dao;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 import org.shen.entity.BaseEntity;
 
 import tk.mybatis.mapper.common.Mapper;
 
-public interface BaseDao<T extends BaseEntity> extends Mapper<T>{
-	public void add(T entity);
+public abstract class BaseDao<T extends BaseEntity> implements Mapper<T>{
+	/**
+	 * 定义一个类反射对象，可以通过这个对象反射调用成员和方法，构造函数
+	 */
+	private Class<T> clazz;
+
+	@SuppressWarnings("unchecked")
+	public BaseDao() {
+		// 获取传递父类的泛型参数类型，但你可以理解成一个集合
+		ParameterizedType type = (ParameterizedType) this.getClass()
+				.getGenericSuperclass();
+		// 从集合中获取所有泛型参数类型，Type是Class的接口类
+		Type[] types = type.getActualTypeArguments();
+		// 得到传递的类类型（这里只有一个泛型）
+		clazz = (Class<T>) types[0];
+	}
 }
